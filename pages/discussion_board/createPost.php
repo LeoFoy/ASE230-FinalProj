5e500a122641ffe5c98901d179da005fb5bba4c7
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-12-12 18:25:14 -0500

    Update createPost.php

--- pages/discussion_board/createPost.php
+++ pages/discussion_board/createPost.php
@@ -1,5 +1,9 @@
 <?php
-    require_once('../../lib/functions.php');
+require_once('../../lib/functions.php');
+require_once('../../lib/db.php');
+
+$discussion_board=query($pdo,'SELECT * FROM discussion_board');
+$discussion_board=$discussion_board->fetchAll();
 ?>
 
 <!DOCTYPE html>
