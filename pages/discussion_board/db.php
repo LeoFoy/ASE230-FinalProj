6b8daf6b8b0cd5843573b6f9a25b325578a6a6e9
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-12-12 16:47:09 -0500

    Update db.php

--- pages/discussion_board/db.php
+++ pages/discussion_board/db.php
@@ -9,7 +9,7 @@ $opt = [
 	PDO::ATTR_EMULATE_PREPARES => false
 ];
 
-$pdo=new PDO($dsn, $discussion_board, $pass, $opt);
+$pdo=new PDO($dsn, $user, $pass, $opt);
 
 function query($pdo,$query,$data=[]) {
 	$query=$pdo->prepare($query);
