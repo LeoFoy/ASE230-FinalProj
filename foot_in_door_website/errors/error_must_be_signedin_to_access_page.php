e303bf6776d979bf692e4edb439bfbe74711d618
LeoFoy <lfoy306@gmail.com>
2023-12-13 16:09:43 -0500

    Merging Julianna Branch continued

---
+++ foot_in_door_website/errors/error_must_be_signedin_to_access_page.php
@@ -0,0 +1,4 @@
+<h1>Must be signed in to access this page!</h1>
+<a href="../login.php">Sign In</a><br />
+<a href="../signup.php">Sign up</a><br />
+<a href="../../foot_in_door_website/index.php">Go to home page</a>
---
+++ foot_in_door_website/errors/error_unable_to_change_username.php
@@ -0,0 +1,3 @@
+<h1>Current username is not correct, unable to change username</h1>
+<a href="../user_profile.php">Go back to profile</a><br />
+<a href="../../foot_in_door_website/index.php">Go back to home page</a>
