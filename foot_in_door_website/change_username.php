d3f3f68a688238be4e44748e64b8a38c6184cff0
Julianna Truitt <truittj3@nku.edu>
2023-12-13 21:23:04 -0500

    fixing error from when i mergered file conflict from git bash

--- foot_in_door_website/change_username.php
+++ foot_in_door_website/change_username.php
@@ -1,15 +1,12 @@
 <?php
 require_once("../settings.php");
-<<<<<<< HEAD
 require_once("../theme/header.php");
-=======
->>>>>>> 098617f4b1ba0ccc21aac443569c025b93e5d4a9
 if (!isset($_SESSION['user_id']) and !isset($_SESSION['username']) and !isset($_SESSION['role'])){
 	header('location: errors/error_must_be_signedin_to_access_page.php');
 	exit();
 }
 ?>
-<<<<<<< HEAD
+
 <header class="bg-dark py-5">
      <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
@@ -29,7 +26,6 @@ if (!isset($_SESSION['user_id']) and !isset($_SESSION['username']) and !isset($_
 <?php
 require_once("../theme/footer.php");
 ?>
-=======
 
 <h1>Change Username</h1>
 <form method="POST" action="../lib/auth/auth.php">
@@ -41,4 +37,4 @@ require_once("../theme/footer.php");
 </form>
 <a href="user_profile.php">Go back to profile</a><br />
 <a href="../foot_in_door_website/index.php">Go to home page</a>
->>>>>>> 098617f4b1ba0ccc21aac443569c025b93e5d4a9
+
