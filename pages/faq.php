4e2a7b077f0100ea5279792df09980f25dd5ab8d
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-12-13 21:15:18 -0500

    Update faq.php

--- pages/faq.php
+++ pages/faq.php
@@ -29,10 +29,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="../foot_in_door_website/index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../pages/create_resume.php">Create Resume</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../pages/resume/create_resume.php">Create Resume</a></li>
 						<li class="nav-item"><a class="nav-link" href="../pages/templates.php">Templates</a></li>
 						<li class="nav-item"><a class="nav-link" href="../pages/tips.php">Resume-Making Tips</a></li>
-						<li class="nav-item"><a class="nav-link" href="../pages/discussion_board.php">Discussion Board</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/discussion_board/discussion_board.php">Discussion Board</a></li>
 						<li class="nav-item"><a class="nav-link" href="../pages/faq.php">FAQ/Support</a></li>
 						<li class="nav-item"><a class="nav-link" href="../pages/contact_us.php">Contact Us</a></li>
                     </ul>
